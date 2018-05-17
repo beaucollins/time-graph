3811063add5eba156381f75a1033ef43ae0effb3
Robert Collins <beau@collins.pub>
2018-05-17 11:17:34 -0700

    Convert from time domain to screen domain

--- src/component/block-graph/index.jsx
+++ src/component/block-graph/index.jsx
@@ -16,10 +16,31 @@ export default class BlockGraph extends Component {
 		 * The coefficient that converts a measument in seconds into a measurement in pixels
 		 */
 		pixelsPerSecond: PropTypes.number.isRequired,
+		/**
+		 * The constant in seconds that defines the "left edge" of the graph
+		 */
+		originSeconds: PropTypes.number.isRequired,
 	};
 
 	static defaultProps = {
 		blocks: [],
+	}
+
+	/**
+	 * @param {number} seconds - a quantity of seconds
+	 * @returns {number} the measurement in pixels that represents the seconds for this graph
+	 */
+	covertSecondsToPixels(seconds) {
+		return this.props.pixelsPerSecond * seconds;
+	}
+
+	/**
+	 * @param {number} seconds - seconds since the epoch
+	 * @returns {number} pixel x value that represents that second in this intance of the graph
+	 */
+	convertAbsoluteSecondsToPixels(seconds) {
+		// figure out
+		return this.convertSecondsToPixels(seconds - this.props.originSeconds);
 	}
 
 	render() {
