a1918d8c812720154a1a0e5c4a146d84e360d667
Robert Collins <beau@collins.pub>
2018-05-17 10:38:45 -0700

    blocks prop

--- src/component/block-graph/index.jsx
+++ src/component/block-graph/index.jsx
@@ -5,7 +5,18 @@ import './style';
 
 export default class BlockGraph extends Component {
 	static propTypes = {
+		/**
+		 * A list of items to draw on the screen
+		 */
+		blocks: PropTypes.arrayOf(PropTypes.shape({
+			startTime: PropTypes.number.isRequired,
+			endTime: PropTypes.number.isRequired,
+		})).isRequired,
 	};
+
+	static defaultProps = {
+		blocks: [],
+	}
 
 	render() {
 		return <div className="block-graph">Hello World</div>;
