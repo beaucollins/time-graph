e954553930ffd0ccec0eb28b417c649c6af2222b
Robert Collins <beau@collins.pub>
2018-05-17 10:59:49 -0700

    An hour will take up 100 pixels, let it be so

--- src/component/demo.jsx
+++ src/component/demo.jsx
@@ -1,6 +1,9 @@
 import { Component } from 'react';
 import BlockGraph from 'component/block-graph';
 
+const HOUR_IN_PIXELS = 100;
+const SECONDS_PER_HOUR = 60 * 60;
+
 export default class Demo extends React.Component {
 	render() {
 		return <div id="demo">
@@ -8,6 +11,7 @@ export default class Demo extends React.Component {
 			</div>
 			<div id="graph-container">
 				<BlockGraph
+					pixelsPerSecond={HOUR_IN_PIXELS / SECONDS_PER_HOUR}
 				/>
 			</div>
 		</div>;
